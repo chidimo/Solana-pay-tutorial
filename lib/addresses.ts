b07ef1e88ac1e6ddb50d1e1184a0fa0c29be0149
Chidi Orji <orjichidi95@gmail.com>
2022-04-11 00:51:15 +0100

    chore: finish solana pay

---
+++ lib/addresses.ts
@@ -0,0 +1,13 @@
+import { PublicKey } from '@solana/web3.js'
+
+const solWalletAddress = ''
+
+// Your shop wallet address
+export const shopAddress = new PublicKey(
+  '5zcCFrV96Lux1U937mQttJ4Gt6f8g8qAFWGdwiovRsKT'
+)
+
+// this is the same for everyone!
+export const usdcAddress = new PublicKey(
+  'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
+)

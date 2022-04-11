28afc63019c4d498a8b847d4adc80779ca46f5eb
Chidi Orji <orjichidi95@gmail.com>
2022-04-11 01:56:15 +0100

    feat: implement loyalty program

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -11,3 +11,8 @@ export const shopAddress = new PublicKey(
 export const usdcAddress = new PublicKey(
   'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
 )
+
+// This is your token/coupon address
+export const couponAddress = new PublicKey(
+  'J3Nsp8jEvqCkEXUJn4FzEw8j8obqTgLeS42w4UGa9HgZ'
+)
